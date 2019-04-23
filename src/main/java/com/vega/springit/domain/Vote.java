52f830c4909568bb2bf2962e3c2799197d201ac7
nsc <nsc@bi-web.de>
2019-04-23 15:31:02 +0200

    - voting

--- src/main/java/com/vega/springit/domain/Vote.java
+++ src/main/java/com/vega/springit/domain/Vote.java
@@ -1,24 +1,32 @@
 package com.vega.springit.domain;
 
-import lombok.*;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @NoArgsConstructor
+@RequiredArgsConstructor
 @Getter
 @Setter
 @ToString
 public class Vote extends Auditable {
 
-    @Id
-    @GeneratedValue
-    private Long id;
-    private int vote;
+  @Id
+  @GeneratedValue
+  private Long id;
 
-    // user
-    // link
+  @NonNull
+  private short direction;
 
+  @NonNull
+  @ManyToOne
+  private Link link;
 }
